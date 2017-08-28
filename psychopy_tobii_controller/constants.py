4b1f3e3b3b465bdb0805a2535b38429c40b9521e
Hiroyuki Sogo <hsogo12600@gmail.com>
2017-08-28 17:47:05 +0900

    NEW: add psychopy_tobii_controller.constants module.

---
+++ psychopy_tobii_controller/constants.py
@@ -0,0 +1,18 @@
+#
+# Tobii controller for PsychoPy
+# 
+# author: Hiroyuki Sogo
+# Distributed under the terms of the GNU General Public License v3 (GPLv3).
+# 
+
+TimeStamp = 0
+GazePointXLeft = 1
+GazePointYLeft = 2
+PupilLeft = 3
+ValidityLeft = 4
+GazePointXRight = 5
+GazePointYRight = 6
+PupilRight = 7
+ValidityRight = 8
+GazePointX = 9
+GazePointY = 10
