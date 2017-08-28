014784828c76d125d452773c3470561e6d731674
Hiroyuki Sogo <hsogo12600@gmail.com>
2017-08-28 20:54:25 +0900

    NEW: add utility_sample01.py

---
+++ samples/utility_sample01.py
@@ -0,0 +1,70 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# import utility
+import psychopy_tobii_controller.utility as util
+import psychopy_tobii_controller.constants as const
+
+# Load data file using load_data().
+# Return values are two list objects.
+# The first one is gaze data, and the other is event data.
+gaze_data, event_data = util.load_data('data.tsv')
+
+# Use moving_average() to smooth gaze data if necessary.
+# Note that the first argument of moving_average() is not
+# gaze data list returned by load_data, but the ELEMENTS of 
+# the gaze data.
+# 
+# If you want to apply moving average to N-th recording session
+# of the gaze data, call moving_average()
+# 
+#     util.moving_average(gaze_data[N], n=3)
+# 
+smoothed_data = [util.moving_average(g, n=3) for g in gaze_data]
+
+
+for trial in range(len(gaze_data)):
+
+    # Getting list of fixations using detect_fixation_IDT().
+    # numpy.savetxt() would be convenient to output fixation data
+    # to a text file.
+    #
+    #     numpy.savetxt('fix.txt',fixations, fmt='%.1f', delimiter=',')
+    #
+    fixations = util.detect_fixation_IDT(smoothed_data[trial],
+                                         max_dispersion = 50,
+                                         min_duration = 100,
+                                         eye = 'LR')
+    
+    # Prepare gaze data to plot.
+    t = smoothed_data[trial][:,const.TimeStamp]
+    x = smoothed_data[trial][:,const.GazePointX]
+    y = smoothed_data[trial][:,const.GazePointY]
+    
+    # X-Y plot (smoothed data)
+    plt.subplot(2,2,1)
+    plt.xlim([-800,800])
+    plt.ylim([-600,600])
+    plt.plot(x, y)
+    
+    # X-Y plot (detected fixations)
+    plt.subplot(2,2,2)
+    plt.xlim([-800,800])
+    plt.ylim([-600,600])
+    plt.plot(fixations[:,const.FixX], fixations[:,const.FixY], 'o-')
+    
+    # X-T plot with event data
+    plt.subplot(2,2,3)
+    plt.plot(t, x, label='X')
+    plt.plot(t, y, label='Y')
+    plt.legend()
+    
+    for event in event_data[trial]:
+        plt.plot([event[const.EventTime], event[const.EventTime]],
+                 [-800, 800], 'k:')
+        plt.text(event[const.EventTime], 400, event[const.EventText],
+                 rotation=90)
+    plt.ylim([-800,800])
+    
+    plt.show()
+
