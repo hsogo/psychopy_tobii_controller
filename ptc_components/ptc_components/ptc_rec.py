5ac08a23f9e4bbc504b6db9119a48b1ddb36b81b
Hiroyuki Sogo <hsogo12600@gmail.com>
2022-04-22 19:10:18 +0900

    ENH: support for "target", recording over multiple routines

--- ptc_components/ptc_components/ptc_rec.py
+++ ptc_components/ptc_components/ptc_rec.py
@@ -7,24 +7,34 @@
 from psychopy.experiment.components import BaseComponent, Param
 from os import path
 
-thisFolder = path.abspath(path.dirname(__file__))#the absolute path to the folder containing this path
-iconFile = path.join(thisFolder,'ptc_rec.png')
-tooltip = 'ptc_rec: tobii_controller calibration'
 
 class ptc_rec_component(BaseComponent):
     """Recording"""
     categories = ['tobii_controller']
-    def __init__(self, exp, parentName, name='ptc_rec', start_msg='', stop_msg=''):
+    targets = ['PsychoPy']
+    thisFolder = path.abspath(path.dirname(__file__))#the absolute path to the folder containing this path
+    iconFile = path.join(thisFolder,'ptc_rec.png')
+    tooltip = 'ptc_rec: tobii_controller calibration'
+
+    def __init__(self, exp, parentName, name='ptc_rec', start_rec=True, start_msg='', stop_rec=True, stop_msg=''):
         super(ptc_rec_component, self).__init__(exp, parentName, name)
         self.type='ptc_rec'
         self.url='https://github.com/hsogo/psychopy_tobii_controller/'
         
         #params
-        self.order = ['name', 'start_msg', 'stop_msg']
+        self.order = ['name', 'start_rec', 'start_msg', 'stop_rec', 'stop_msg']
+        self.params['start_rec']=Param(start_rec, valType='bool', allowedTypes=[],
+            updates='constant', allowedUpdates=[],
+            hint="Start recording at the beginning of this routine.  Uncheck this if recording is continuing from the preceding routine.",
+            label="Start Recording")
         self.params['start_msg'] = Param(start_msg, valType='code', allowedTypes=[],
             updates='constant', allowedUpdates=[],
             hint='This text is inserted at the beginning of the recording.',
             label='Event (start)', categ='Advanced')
+        self.params['stop_rec']=Param(stop_rec, valType='bool', allowedTypes=[],
+            updates='constant', allowedUpdates=[],
+            hint="Stop recording at the end of this routine.  Uncheck this if you want to continue recording in the next routine.",
+            label="Stop Recording")
         self.params['stop_msg'] = Param(stop_msg, valType='code', allowedTypes=[],
             updates='constant', allowedUpdates=[],
             hint='This text is inserted at the end of the recording.',
@@ -38,14 +48,16 @@ class ptc_rec_component(BaseComponent):
                 del self.params[p]
 
     def writeRoutineStartCode(self, buff):
-        buff.writeIndented('ptc_controller_tobii_controller.subscribe()\n')
-        if self.params['start_msg'].val != '':
-            buff.writeIndented('ptc_controller_tobii_controller.record_event(%(start_msg)s)\n' % (self.params))
+        if self.params['start_rec'].val:
+            buff.writeIndented('ptc_controller_tobii_controller.subscribe()\n')
+            if self.params['start_msg'].val != '':
+                buff.writeIndented('ptc_controller_tobii_controller.record_event(%(start_msg)s)\n' % (self.params))
 
     def writeFrameCode(self, buff):
         pass
 
     def writeRoutineEndCode(self,buff):
-        if self.params['stop_msg'].val != '':
-            buff.writeIndented('ptc_controller_tobii_controller.record_event(%(stop_msg)s)\n' % (self.params))
-        buff.writeIndented('ptc_controller_tobii_controller.unsubscribe()\n')
+        if self.params['stop_rec'].val:
+            if self.params['stop_msg'].val != '':
+                buff.writeIndented('ptc_controller_tobii_controller.record_event(%(stop_msg)s)\n' % (self.params))
+            buff.writeIndented('ptc_controller_tobii_controller.unsubscribe()\n')
