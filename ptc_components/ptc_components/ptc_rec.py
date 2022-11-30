b23f77c4b84fe4bfd35223f0f149f1c062891109
Hiroyuki Sogo <hsogo12600@gmail.com>
2022-11-30 19:25:02 +0900

    NF: add "wait" parameter to ptc_rec component

--- ptc_components/ptc_components/ptc_rec.py
+++ ptc_components/ptc_components/ptc_rec.py
@@ -16,13 +16,13 @@ class ptc_rec_component(BaseComponent):
     iconFile = path.join(thisFolder,'ptc_rec.png')
     tooltip = 'ptc_rec: tobii_controller calibration'
 
-    def __init__(self, exp, parentName, name='ptc_rec', start_rec=True, start_msg='', stop_rec=True, stop_msg=''):
+    def __init__(self, exp, parentName, name='ptc_rec', start_rec=True, start_msg='', stop_rec=True, stop_msg='', wait=True):
         super(ptc_rec_component, self).__init__(exp, parentName, name)
         self.type='ptc_rec'
         self.url='https://github.com/hsogo/psychopy_tobii_controller/'
         
         #params
-        self.order = ['name', 'start_rec', 'start_msg', 'stop_rec', 'stop_msg']
+        self.order = ['name', 'start_rec', 'start_msg', 'stop_rec', 'stop_msg', 'wait']
         self.params['start_rec']=Param(start_rec, valType='bool', allowedTypes=[],
             updates='constant', allowedUpdates=[],
             hint="Start recording at the beginning of this routine.  Uncheck this if recording is continuing from the preceding routine.",
@@ -39,6 +39,10 @@ class ptc_rec_component(BaseComponent):
             updates='constant', allowedUpdates=[],
             hint='This text is inserted at the end of the recording.',
             label='Event (stop)', categ='Advanced')
+        self.params['wait'] = Param(wait, valType='bool', allowedTypes=[],
+            updates='constant', allowedUpdates=[],
+            hint='If checked, experiment stops until data become available.',
+            label='Wait for data', categ='Advanced')
             
         # these inherited params are harmless but might as well trim:
         for p in ('startType', 'startVal', 'startEstim', 'stopVal',
@@ -49,7 +53,9 @@ class ptc_rec_component(BaseComponent):
 
     def writeRoutineStartCode(self, buff):
         if self.params['start_rec'].val:
-            buff.writeIndented('ptc_controller_tobii_controller.subscribe()\n')
+            buff.writeIndented('ptc_controller_current_routineTimer_value = routineTimer.getTime()\n')
+            buff.writeIndented('ptc_controller_tobii_controller.subscribe(wait=%(wait)s)\n' % (self.params))
+            buff.writeIndented('routineTimer.reset(ptc_controller_current_routineTimer_value)\n')
             if self.params['start_msg'].val != '':
                 buff.writeIndented('ptc_controller_tobii_controller.record_event(%(start_msg)s)\n' % (self.params))
 
